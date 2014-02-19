e100c92c78011da766186a1d0dfbfa883d7ba394
wesmadrigal <wesley7879@gmail.com>
2014-02-18 23:56:36 -0600

    adding install

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='sockproc',
+      version='1.0',
+      author='Wes Madrigal',
+      author_email='wesley7879@gmail.com',
+      description='A mini parallel library',
+      #py_modules=['serversock', 'spawn']
+      packages=['sockproc'],
+)
+   
